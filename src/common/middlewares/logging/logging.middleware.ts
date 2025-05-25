bf852a094251adedb75b06b642702b8361a83fe7
Andrey Kotlyar <kotlyar562@gmail.com>
2025-05-25 20:15:04 +0300

    console.log body only in development mode

--- src/common/middlewares/logging/logging.middleware.ts
+++ src/common/middlewares/logging/logging.middleware.ts
@@ -3,7 +3,8 @@ import { NextFunction, Request, Response } from 'express';
 export function logger(req: Request, res: Response, next: NextFunction) {
   res.on('finish', () => {
     console.info(new Date(), req.url, res.statusCode, res.statusMessage);
-    if (req.body) console.log(req.body);
+    if (req.body && process.env.NODE_ENV === 'development')
+      console.log(req.body);
   });
 
   next();
